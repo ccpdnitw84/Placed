f54c5ac84018a81652a5b6a0a97efeddc90839a6
Abhishek Chand <abhishekchand987@gmail.com>
2024-06-03 12:22:20 +0530

    just checking

--- backend/index.js
+++ backend/index.js
@@ -14,105 +14,108 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's origin
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("MongoDB Connected.");
-        app.listen(PORT, () =>
-            console.log(`Server is running on port ${PORT}`)
-        );
-    })
-    .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected.");
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
 
-app.get('/', (req, res) => res.send("API Running"));
+app.get("/", (req, res) => res.send("API Running"));
 
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 };
 
 const authenticateToken = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) return res.sendStatus(401);
+  const token = req.cookies.token;
+  if (!token) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 };
 
-app.post('/register', async (req, res) => {
-    const {username, email, mobile, password, role} = req.body;
-    User.findOne({ email })
-        .then(existingUser => {
-            if (existingUser) {
-                return res.status(400).json({ message: 'User already has an account' });
-            } else {
-                const newUser = new User({ username, email, mobile, password, role });
-                newUser.save()
-                    .then(() => {
-                        console.log("User registered successfully");
-                        const token = generateToken(newUser);
-                        res.cookie('token', token, { httpOnly: true });
-                        return res.status(200).json({ user: newUser });
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        return res.status(500).send("Error registering user");
-                    });
-            }
-        })
-        .catch(err => {
+app.post("/register", async (req, res) => {
+  const { username, email, mobile, password, role } = req.body;
+  User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(400).json({ message: "User already has an account" });
+      } else {
+        const newUser = new User({ username, email, mobile, password, role });
+        newUser
+          .save()
+          .then(() => {
+            console.log("User registered successfully");
+            const token = generateToken(newUser);
+            res.cookie("token", token, { httpOnly: true });
+            return res.status(200).json({ user: newUser });
+          })
+          .catch((err) => {
             console.error(err);
-            return res.status(500).send("Error checking existing user");
-        });
-});
-
-app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    User.findOne({email})
-        .then(user => {
-            if(user && user.isValidPassword(password)) {
-                console.log("Login Successful");
-                const token = generateToken(user);
-                res.cookie('token', token, {httpOnly: true})
-                return res.status(200).json({ user });   
-            }
-            else {
-                return res.status(401).json({ message: 'Invalid credentials' });
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error logging in");
-        });
-});
-
-app.get('/current-user', (req, res) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
-        }
-        User.findById(decoded.id)
-            .then(user => {
-                if (!user) {
-                    return res.status(404).json({ message: 'User not found' });
-                }
-                return res.status(200).json({ user });
-            })
-            .catch(err => {
-                console.error(err);
-                return res.status(500).send("Error fetching user");
-            });
+            return res.status(500).send("Error registering user");
+          });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send("Error checking existing user");
     });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user && user.isValidPassword(password)) {
+        console.log("Login Successful");
+        const token = generateToken(user);
+        res.cookie("token", token, { httpOnly: true });
+        return res.status(200).json({ user });
+      } else {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error logging in");
+    });
+});
+
+app.get("/current-user", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    User.findById(decoded.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json({ user });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).send("Error fetching user");
+      });
+  });
 });
